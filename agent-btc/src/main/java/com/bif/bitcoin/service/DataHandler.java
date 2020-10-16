0eace36d4dcf5d0b38935fb5daaae32b2023a21a
moses <moses7625@aliyun.com>
2020-10-16 20:20:16 +0800

    按需求整理BTC接口及方法，并标记未提供的chain接口

--- agent-btc/src/main/java/com/bif/bitcoin/service/DataHandler.java
+++ agent-btc/src/main/java/com/bif/bitcoin/service/DataHandler.java
@@ -11,6 +11,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import wf.bitcoin.javabitcoindrpcclient.BitcoindRpcClient;
 
+import java.math.BigDecimal;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -61,8 +62,41 @@ public class DataHandler extends BaseDataHandler {
             //System.out.println(response.toString());
             System.out.println(request);
             System.out.println("................................");
+            this.tmp();
         } catch (Exception e) {
             logger.error("", e);
         }
+    }
+
+    /**
+     * NOTE: 对照需求列出所需要的链接口
+     */
+    private void tmp() {
+        // 1 获取最新区块hash
+        String lastBlockHash = btcClient.getBestBlockHash();
+        // 2 获取最新区块数
+        int blockCount = btcClient.getBlockCount();
+        // 3 获取某区块高度的区块Hash
+        String blockHash = btcClient.getBlockHash(100);
+        // 4 获取区块信息(高度 or Hash)
+        BitcoindRpcClient.Block block = btcClient.getBlock(100);
+        // 5 获取交易信息...
+        // txId: The TXID of the transaction containing the output to get, encoded as hex in RPC byte order
+        String txId = "";
+        BitcoindRpcClient.RawTransaction rawTransaction = btcClient.getRawTransaction(txId);
+        // 6. 获取区块链链接数
+        long connCount = btcClient.getConnectionCount();
+        // 7. 获取算力难度
+        BigDecimal difficulty = btcClient.getDifficulty();
+        // 8. Missing获取链交易状态数据(getchaintxstats)
+        // 9. 获取区块链概览信息
+        BitcoindRpcClient.BlockChainInfo blockChainInfo = btcClient.getBlockChainInfo();
+        // 10. Missing获取内存使用(getmempoolinfo)
+        // 11. none获取当前节点信息(getnodeaddresses)
+        // 12. 获取utxo信息
+        txId = "";
+        // vout : The output index number (vout) of the output within the transaction
+        long vout = 1;
+        BitcoindRpcClient.TxOut txOut = btcClient.getTxOut(txId, vout);
     }
 }
