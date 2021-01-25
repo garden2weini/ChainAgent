e1395e9b19bb8056e253b10ca6514dfa94b3b955
moses <moses7625@aliyun.com>
2021-01-25 18:10:57 +0800

    restTemplate支持访问https

--- agent-eos/src/main/java/com/bif/eos/client/EosClient.java
+++ agent-eos/src/main/java/com/bif/eos/client/EosClient.java
@@ -3,14 +3,22 @@ package com.bif.eos.client;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.bif.eos.vm.Block;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * EOS接口封装
@@ -18,12 +26,37 @@ import java.util.Map;
  */
 public class EosClient {
     private String address;
-    RestTemplate restTemplate = new RestTemplate();
+    RestTemplate restTemplate; // = new RestTemplate();
+
     // curl http://127.0.0.1:8888/v1/chain/get_info
     private static final String GET_BLOCK = "/v1/chain/get_block";
 
+    private CloseableHttpClient httpClient()
+    {
+        // 支持HTTP、HTTPS
+        Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory> create()
+                .register("http", PlainConnectionSocketFactory.getSocketFactory())
+                .register("https", SSLConnectionSocketFactory.getSocketFactory())
+                .build();
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager(registry);
+        connectionManager.setMaxTotal(200);
+        connectionManager.setDefaultMaxPerRoute(100);
+        connectionManager.setValidateAfterInactivity(2000);
+        RequestConfig requestConfig = RequestConfig.custom()
+                // 服务器返回数据(response)的时间，超时抛出read timeout
+                .setSocketTimeout(65000)
+                // 连接上服务器(握手成功)的时间，超时抛出connect timeout
+                .setConnectTimeout(5000)
+                // 从连接池中获取连接的超时时间，超时抛出ConnectionPoolTimeoutException
+                .setConnectionRequestTimeout(1000)
+                .build();
+        return HttpClientBuilder.create().setDefaultRequestConfig(requestConfig).setConnectionManager(connectionManager).build();
+    }
+
     public EosClient(String address) {
         this.address = address;
+        ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient());
+        this.restTemplate = new RestTemplate(requestFactory);
     }
 
     /**
